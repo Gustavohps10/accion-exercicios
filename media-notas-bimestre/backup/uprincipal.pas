b2e269b11afd9153be5163f81b338905665722f6
Gustavo Henrique <gustavoh.santos735@gmail.com>
2024-03-21 00:21:35 -0300

    improve - avg scores

--- media-notas-bimestre/backup/uprincipal.pas
+++ media-notas-bimestre/backup/uprincipal.pas
@@ -5,7 +5,9 @@ unit uPrincipal;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls,
+  StrUtils,
+  Math;
 
 type
 
@@ -25,11 +27,11 @@ type
     lblNota2: TLabel;
     lblNota3: TLabel;
     lblNota4: TLabel;
+    pnlTopo: TPanel;
     procedure btnCalcularClick(Sender: TObject);
   private
 
   public
-     function calculaMedia(notas: array of double): double;
 
   end;
 
@@ -38,7 +40,7 @@ var
 
 const
   mediaAprovacao = 7;
-  maxFalta = 25;
+  maxFaltas = 25;
 
 implementation
 
@@ -48,39 +50,15 @@ implementation
 
 procedure TPrincipalF.btnCalcularClick(Sender: TObject);
 var notas: array of double;
-var media: double;
 var faltas: integer;
+var message: string;
 begin
+    notas := [StrToFloat(edtNota1.text),StrToFloat(edtNota2.text),StrToFloat(edtNota3.text),StrToFloat(edtNota4.text)];
     faltas := strtoint(edtFaltas.text);
-    notas := [
-          StrToFloat(edtNota1.text),
-          StrToFloat(edtNota2.text),
-          StrToFloat(edtNota3.text),
-          StrToFloat(edtNota4.text)];
-    media := calculaMedia(notas);
-    edtMedia.text := FloatToStr(media);
-
-    if (faltas > maxFalta) or (media < mediaAprovacao) then
-    begin
-      ShowMessage('Reprovado');
-      Exit;
-    end;
-
-    ShowMessage('Aprovado');
+    edtMedia.text := FloatToStr(Mean(notas));
+    message := ifThen((faltas > maxFaltas) or (Mean(notas) < mediaAprovacao),'Reprovado','Aprovado');
+    ShowMessage(message);
 end;
-
-function TprincipalF.calculaMedia(notas: array of Double): Double;
-var totalNotas: double = 0;
-var nota: double;
-begin
-    for nota in notas do
-    begin
-       totalNotas := totalNotas + nota;
-    end;
-
-    Result := totalNotas / Length(notas);
-end;
-
 
 end.
 
