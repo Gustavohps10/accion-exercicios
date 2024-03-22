aec37ff1c29cb50a45352348fbbf036d6b0e3587
Gustavo Henrique <gustavoh.santos735@gmail.com>
2024-03-22 16:45:34 -0300

    exercicio - contagem regressiva

---
+++ modulo 1/aula 3/exercicio 1 - contagem regressiva/backup/uprincipal.pas
@@ -0,0 +1,58 @@
+unit uPrincipal;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls;
+
+type
+
+  { TprincipalF }
+
+  TprincipalF = class(TForm)
+    btnContar: TButton;
+    edtNumero1: TEdit;
+    edtNumero2: TEdit;
+    lnlNumero1: TLabel;
+    lnlNumero2: TLabel;
+    mmoContagem: TMemo;
+    pnlTopo: TPanel;
+    procedure btnContarClick(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  principalF: TprincipalF;
+
+implementation
+
+{$R *.lfm}
+
+{ TprincipalF }
+
+procedure TprincipalF.btnContarClick(Sender: TObject);
+var n1, n2:integer;
+begin
+  n1 := StrToIntDef(edtNumero1.text, 10);
+  n2 := StrToIntDef(edtNumero2.text, 5);
+  if n1 < n2 then
+  begin
+    ShowMessage('Número 1 é menor que o número 2');
+    Exit;
+  end;
+
+  mmoContagem.Clear;
+  for n1:=n1 downto n2 do
+  begin
+       mmoContagem.Append(inttostr(n1));
+  end;
+
+end;
+
+end.
+
