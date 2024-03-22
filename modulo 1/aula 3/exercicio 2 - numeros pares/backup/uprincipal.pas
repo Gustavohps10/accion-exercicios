55597bbba59dd91b29bc126a71e4acde7938a2d1
Gustavo Henrique <gustavoh.santos735@gmail.com>
2024-03-22 17:25:37 -0300

    exercicio 2

---
+++ modulo 1/aula 3/exercicio 2 - numeros pares/backup/uprincipal.pas
@@ -0,0 +1,52 @@
+unit uPrincipal;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TprincipalF }
+
+  TprincipalF = class(TForm)
+    btnLoop: TButton;
+    mmoPares: TMemo;
+    procedure btnLoopClick(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  principalF: TprincipalF;
+
+implementation
+
+{$R *.lfm}
+
+{ TprincipalF }
+
+procedure TprincipalF.btnLoopClick(Sender: TObject);
+var i: integer = 0;
+  var pares: array of integer;
+begin
+  mmoPares.Clear;
+  while i < 10 do
+  begin
+      if i mod 2 = 0 then
+      begin
+        Insert(i, pares, -1);
+        mmoPares.Append(IntToStr(i));
+        if Length(pares) = 3 then
+           Break;
+      end;
+      i := i+1;
+  end;
+end;
+
+end.
+
