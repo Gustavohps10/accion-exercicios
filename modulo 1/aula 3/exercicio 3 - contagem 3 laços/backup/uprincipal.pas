b49d5a27922b7074af1a1d762042c7e55707ac05
Gustavo Henrique <gustavoh.santos735@gmail.com>
2024-03-23 01:17:35 -0300

    exercicio 3

--- modulo 1/aula 3/exercicio 3 - contagem 3 laços/backup/uprincipal.pas
+++ modulo 1/aula 3/exercicio 3 - contagem 3 laços/backup/uprincipal.pas
@@ -5,7 +5,7 @@ unit uPrincipal;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Grids, StdCtrls,
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls,
   ExtCtrls;
 
 type
@@ -25,7 +25,7 @@ type
   private
 
   public
-
+     procedure escreverImparNoMemo(numero: Integer; memo: TMemo);
   end;
 
 var
@@ -40,21 +40,35 @@ implementation
 procedure TprincipalF.btnMostrarImparesClick(Sender: TObject);
 var i: integer = 0;
 begin
+   mmoImpares1.Clear;
+   mmoImpares2.Clear;
+   mmoImpares3.Clear;
+
    for i := i to 10 do
    begin
-
+      escreverImparNoMemo(i, mmoImpares1);
    end;
 
+   i := 0;
    while i < 10 do
    begin
+       escreverImparNoMemo(i, mmoImpares2);
        i := i + 1
    end;
 
+   i := 0;
    repeat
-         i := i +1;
-         if i
-    until i = 10 ;
+         escreverImparNoMemo(i, mmoImpares3);
+         i := i + 1;
+   until i = 10 ;
+end;
 
+procedure TprincipalF.escreverImparNoMemo(numero: integer; memo: TMemo);
+begin
+   if numero mod 2 <> 0 then
+   begin
+      memo.Append(inttostr(numero));
+   end;
 end;
 
 end.
